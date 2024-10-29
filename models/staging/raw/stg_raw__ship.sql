0e731524e829a55f6e5f05d04f5cb8b11798b328
chloe.fiems@ieseg.fr <chloe.fiems@ieseg.fr>
2024-10-29 13:35:28 +0000

    rename column

---
+++ models/staging/raw/stg_raw__ship.sql
@@ -0,0 +1,22 @@
+with 
+
+source as (
+
+    select * from {{ source('raw', 'ship') }}
+
+),
+
+renamed as (
+
+    select
+        orders_id,
+        shipping_fee,
+        shipping_fee_1,
+        logcost,
+        CAST (ship_cost AS FLOAT64)
+
+    from source
+
+)
+
+select * from renamed
